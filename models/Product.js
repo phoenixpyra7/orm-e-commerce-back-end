3e7b048fac4c9da9fa52486b5aea76667c62844e
Seven <phoenixpyra7@gmail.com>
2024-01-28 20:58:30 -0600

    setting up models, not done

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,9 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    category_id: {
+      type: DataTypes.INTEGER
+    },
     product_name: {
       type: DataTypes.STRING
     },
