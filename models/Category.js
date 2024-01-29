e7494a819bd86fbfaf76ead53348aadce1916afd
Seven <phoenixpyra7@gmail.com>
2024-01-28 21:22:14 -0600

    Defined the category model

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,19 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+//Defined the categorey model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
