68bda3cb9e2f5a1af2760deb07a0bc3135d63d84
Seven <phoenixpyra7@gmail.com>
2024-01-30 17:46:23 -0600

    started tag routes, will return to finish

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,52 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+ // find all tags be sure to include its associated Product data
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({include: [Model, Product, ProductTag]}) // double check if this is right
+  .then(tags => {
+    res.status(200).json(tags)
+    res.json(tags) 
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: 'Error finding tags',
+      error: err,
+    });
+  })
 });
 
   // find a single tag by its `id`
   // be sure to include its associated Product data
 router.get('/:id', async (req, res) => {
-  Tag.findByPk({include: [ID]})
-    res.json(err);
-  });
-  res.json(tagId);
-;
-
+  Tag.findOne({ where: {id: req.params.id},include: [Model, Product, ProductTag]}) // double check if this is right
+  .then(tags => {
+    res.status(200).json(tags)
+    res.json(tags) 
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: 'Error finding tag',
+      error: err,
+    });
+  })
+});
+  // This is to create a new tag
 router.post('/', async (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
+ // update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+ 
+  
 });
 
 router.delete('/:id', async (req, res) => {
