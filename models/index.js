22dbe1b5a62e90d375a9c90f059c91cd1c857a88
Seven <phoenixpyra7@gmail.com>
2024-01-31 17:40:44 -0600

    finished index

--- models/index.js
+++ models/index.js
@@ -1,33 +1,39 @@
 // imported models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Prodeuct.belongsTo(Category, {
-  
-})
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
 
 // Categories have many Products
 Category.hasMany(Product, {
-  
-})
+  foreignKey: "category_id",
+});
 
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  
-})
-
+  through: {
+    model: ProductTag,
+    as: "product_tags",
+    foreignKey: "product_id",
+  },
+});
 
 
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongToMany(Product, {
-  
-})
-
+  through: ProductTag,
+  as: "product_tags",
+  foreignKey: "tag_id",
+});
 
 
 
