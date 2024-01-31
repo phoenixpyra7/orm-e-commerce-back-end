bbe0f4519ab36d4f7c978b3e8c0c2f950621c284
Seven <phoenixpyra7@gmail.com>
2024-01-31 17:26:00 -0600

    build out of index js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,35 @@
-// import models
+// imported models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Prodeuct.belongsTo(Category, {
+  
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  
+})
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  
+})
+
+
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  
+})
+
+
+
 
 module.exports = {
   Product,
