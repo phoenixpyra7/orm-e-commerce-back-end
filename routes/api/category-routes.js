aafa20fb019f83bd359ca4bc5e48a354b556dbf8
Seven <phoenixpyra7@gmail.com>
2024-01-31 21:34:24 -0600

    finished half of the category routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,58 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+
+
+// find all categories be sure to include its associated Products
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({include: [Product]}) 
+  .then(categories => {
+    res.json(categories)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: 'Error finding categories',
+      error: err,
+    });
+  })
 });
 
+
+// find one category by its `id` value be sure to include its associated Products
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id,{include: [Product]})
+  .then(category => {
+    res.json(category)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: 'Error finding category',
+      error: err,
+    });
+  })
+
 });
 
+
+
+ // create a new category
 router.post('/', async (req, res) => {
-  // create a new category
+ 
+  
 });
 
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  
+  
 });
 
-router.delete('/:id', async (req, res) => {
+
+
   // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+
+  
 });
 
 module.exports = router;
