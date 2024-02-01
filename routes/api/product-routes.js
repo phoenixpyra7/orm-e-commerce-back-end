b3bab1c8e4707fd15803e1f5ffc845cf10acf9c1
Seven <phoenixpyra7@gmail.com>
2024-02-01 17:20:48 -0600

    tried to fix code issue

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -35,30 +35,8 @@ router.get('/:id', async (req, res) => {
   });
 
 // This is to create new product
+// create new product
 router.post('/', async (req, res) => {
-  req.body.price = req.body.price * 100
-  {
-    Product.create(req.body)
-      .then((product) => {
-        // This says if there are product tags, then we would need to create pairings to bulk create.
-        if (req.body.tagIds.length) {
-          const productTagIdArr = req.body.tagIds.map((tag_id) => {
-            return {
-              product_id: product.id,
-              tag_id,
-            };
-          });
-          return ProductTag.bulkCreate(productTagIdArr);
-        }
-        // This says if no product tags, just respond
-        res.status(200).json(product);
-      })
-      .then((productTagIds) => res.status(200).json(productTagIds))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -69,13 +47,13 @@ router.post('/', async (req, res) => {
   */
   try {
     const product = await Product.create(req.body);
-    // This says if there are product tags, we need to create pairings by using the setTags method
+    // if there's product tags, we need to create pairings by using the setTags method
     if (req.body.tagIds) {
       await product.setTags(req.body.tagIds);
       await product.save();
       return res.status(200).json(await product.getTags());
     }
-    // TThis says if no product tags, just respond
+    // if no product tags, just respond
     return res.status(200).json(product);
   } catch (err) {
     console.log(err);
@@ -83,7 +61,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-// This is to update the product
+// update product
 router.put('/:id', async (req, res) => {
   try {
     const product = await Product.findByPk(req.params.id, { 
@@ -91,7 +69,7 @@ router.put('/:id', async (req, res) => {
     });
     // update product data
     product.update(req.body);
-    // This says if there are product tags, we need to create pairings by using the setTags method
+    // if there's product tags, we need to create pairings by using the setTags method
     if (req.body.tagIds) {
       await product.setTags(req.body.tagIds);
     }
@@ -103,6 +81,54 @@ router.put('/:id', async (req, res) => {
     return res.status(500).json(err);
   }
 });
+// This is to update the product
+// create new product
+router.post('/', async (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+  try {
+    const product = await Product.create(req.body);
+    // if there's product tags, we need to create pairings by using the setTags method
+    if (req.body.tagIds) {
+      await product.setTags(req.body.tagIds);
+      await product.save();
+      return res.status(200).json(await product.getTags());
+    }
+    // if no product tags, just respond
+    return res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// update product
+router.put('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, { 
+      include: [Tag],
+    });
+    // update product data
+    product.update(req.body);
+    // if there's product tags, we need to create pairings by using the setTags method
+    if (req.body.tagIds) {
+      await product.setTags(req.body.tagIds);
+    }
+    await product.save();
+    await product.reload();
+    return  res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
 // This is to delete one product by its `id` value // check if i did thhis correct
 // I used the async/await syntax for this block to practice.
 router.delete('/:id', async (req, res) => {
