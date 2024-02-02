95bcb2e0b678f075514b8bf719eccd9f3754354a
Seven <phoenixpyra7@gmail.com>
2024-02-01 21:09:14 -0600

    had to find and fix bug in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
 
   // find a single tag by its `id` be sure to include its associated Product data
 router.get('/:id', async (req, res) => {
-  Tag.findOne({ where: {id: req.params.id},include: [Product, ProductTag]}) // double check if this is right
+  Tag.findOne({ where: {id: req.params.id},include: [{model:Product, through: ProductTag}]}) // double check if this is right
   .then(tags => {
     res.status(200).json(tags)
     res.json(tags) 
@@ -50,7 +50,11 @@ router.post('/', async (req, res) => {
 
  // This is to update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
- Tag.update(req.body)
+ Tag.update(req.body, {
+    where: {
+    id: req.params.id
+   }
+ })
     .then((tag) => {
       res.status(200).json(tag);
     })
