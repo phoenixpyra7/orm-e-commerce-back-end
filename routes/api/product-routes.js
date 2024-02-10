c685713ed95632bfce9a54858e4f0137ea26d9c5
Seven <phoenixpyra7@gmail.com>
2024-02-10 12:53:00 -0600

    removed duplicate code in the product routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -81,56 +81,9 @@ router.put('/:id', async (req, res) => {
     return res.status(500).json(err);
   }
 });
-// This is to update the product
-// create new product
-router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  try {
-    const product = await Product.create(req.body);
-    // if there's product tags, we need to create pairings by using the setTags method
-    if (req.body.tagIds) {
-      await product.setTags(req.body.tagIds);
-      await product.save();
-      return res.status(200).json(await product.getTags());
-    }
-    // if no product tags, just respond
-    return res.status(200).json(product);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
 
-// update product
-router.put('/:id', async (req, res) => {
-  try {
-    const product = await Product.findByPk(req.params.id, { 
-      include: [Tag],
-    });
-    // update product data
-    product.update(req.body);
-    // if there's product tags, we need to create pairings by using the setTags method
-    if (req.body.tagIds) {
-      await product.setTags(req.body.tagIds);
-    }
-    await product.save();
-    await product.reload();
-    return  res.status(200).json(product);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
-
-// This is to delete one product by its `id` value // check if i did thhis correct
-// I used the async/await syntax for this block to practice.
+// This is to delete one product by its `id` value
+// I used the async/await syntax for this block to practice
 router.delete('/:id', async (req, res) => {
   try {
       const productData = await Product.destroy({
